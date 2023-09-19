edf7014930c980c1f8fde74b47facef76ce33991
JoGMG <joattah3@gmail.com>
2023-09-19 04:10:21 +0100

    fixed

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ Views module
 """
+from status_server.api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-from status_server.api.v1.views.index import *
